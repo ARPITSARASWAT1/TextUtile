b3ec83f948c2a11780c145bc0bdd608316e3fad8
ARPITSARASWAT1 <113132431+ARPITSARASWAT1@users.noreply.github.com>
2023-05-12 00:14:53 +0530

    Updates

---
+++ precache-manifest.111590134cafb32392ba1dcc1c4db724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "467765ed45bb4b933611",
+    "url": "/TextUtile/static/css/main.356cdfdf.chunk.css"
+  },
+  {
+    "revision": "467765ed45bb4b933611",
+    "url": "/TextUtile/static/js/main.467765ed.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtile/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtile/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "17df45a9308f3b200346",
+    "url": "/TextUtile/static/js/runtime~main.17df45a9.js"
+  },
+  {
+    "revision": "abb123695cc1e4b49cdcb531be08bd41",
+    "url": "/TextUtile/index.html"
+  }
+];
